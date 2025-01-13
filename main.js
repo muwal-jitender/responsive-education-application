f9f505ce079beceb24d505f275dd78291d583f3e
jitender <muwal.jitender@gmail.com>
2025-01-13 13:54:28 +0100

    feat: Add the FAQs sections

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+// Show/Hide faq answer
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+    // Change icon
+    const icon = faq.querySelector(".faq__icon span");
+    icon.textContent = icon.textContent === "add" ? "remove" : "add";
+  });
+});
