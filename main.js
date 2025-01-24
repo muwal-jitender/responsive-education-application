7ba6c78adec8ee43e335add030d52518d043bee0
jitender <muwal.jitender@gmail.com>
2025-01-24 15:20:33 +0100

    fix: Fix the issue that hamburger icon to open the navbar on smaller screen is not working.

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 // Add header layout
 fetch("header.html")
   .then((response) => response.text())
-  .then((data) => (document.getElementById("header").innerHTML = data));
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+    // Change navbar styles on scroll
+    window.addEventListener("scroll", () => {
+      document
+        .querySelector("nav")
+        .classList.toggle("window-scroll", window.scrollY > 0);
+    });
+
+    // Show/Hide NAV Menu on smaller screen
+    const navMenu = document.querySelector(".nav__menu");
+    const openNavMenuBtn = document.querySelector("#open-menu-btn");
+    const closeNavMenuBtn = document.querySelector("#close-menu-btn");
+
+    openNavMenuBtn.addEventListener("click", () => {
+      navMenu.style.display = "flex";
+      closeNavMenuBtn.style.display = "inline-block";
+      openNavMenuBtn.style.display = "none";
+    });
+
+    closeNavMenuBtn.addEventListener("click", () => {
+      navMenu.style.display = "none";
+      closeNavMenuBtn.style.display = "none";
+      openNavMenuBtn.style.display = "inline-block";
+    });
+  });
 
 // Add footer layout
 fetch("footer.html")
   .then((response) => response.text())
   .then((data) => (document.getElementById("footer").innerHTML = data));
-
-// Change navbar styles on scroll
-window.addEventListener("scroll", () => {
-  document
-    .querySelector("nav")
-    .classList.toggle("window-scroll", window.scrollY > 0);
-});
 
 // Show/Hide faq answer
 const faqs = document.querySelectorAll(".faq");
@@ -24,21 +42,4 @@ faqs.forEach((faq) => {
     const icon = faq.querySelector(".faq__icon span");
     icon.textContent = icon.textContent === "add" ? "remove" : "add";
   });
-});
-
-// Show/Hide NAV Menu on smaller screen
-const navMenu = document.querySelector(".nav__menu");
-const openNavMenuBtn = document.querySelector("#open-menu-btn");
-const closeNavMenuBtn = document.querySelector("#close-menu-btn");
-
-openNavMenuBtn.addEventListener("click", () => {
-  navMenu.style.display = "flex";
-  closeNavMenuBtn.style.display = "inline-block";
-  openNavMenuBtn.style.display = "none";
-});
-
-closeNavMenuBtn.addEventListener("click", () => {
-  navMenu.style.display = "none";
-  closeNavMenuBtn.style.display = "none";
-  openNavMenuBtn.style.display = "inline-block";
 });
